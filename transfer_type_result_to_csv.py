c74af9a83c6bb10df55dbec853f34c7c41b3dc59
Hezhi Wang <hw1567@nyu.edu>
2017-04-11 22:36:48 -0400

    transfer type results to csv file

---
+++ transfer_type_result_to_csv.py
@@ -0,0 +1,33 @@
+import pandas as pd
+column_name = []
+valid_number = []
+null_number = []
+Invalid_number = []
+non_empty_missing_data_number = []
+
+f_read = open("type_result.txt", 'r')
+#f_write = open("type_result.csv", 'w')
+
+column_index = 0
+result = [[] for i in range(52)]
+for lines in f_read.readlines():
+	if lines.startswith('Column'):
+		result[int(column_index)] = [0] * 5
+		result[int(column_index)][0] = lines[8:]
+	if lines.startswith('Valid'):
+		lines = lines.replace('\t', '').strip()
+		result[int(column_index)][1] = lines[5:]
+	if lines.startswith('NULL'):
+		lines = lines.replace('\t', '').strip()
+		result[int(column_index)][2] = lines[4:]	
+	if lines.startswith('Invalid'):
+		lines = lines.replace(' ', '').strip()
+		result[int(column_index)][3] = lines[7:]
+	if lines.startswith('Non-empty'):
+		lines = lines.replace(' ', '').strip()
+		result[int(column_index)][4] = lines[20:]
+	if lines == '\n':
+		column_index += 0.5
+re = pd.DataFrame(result)
+re.to_csv('type_result.csv')
+f_read.close()
