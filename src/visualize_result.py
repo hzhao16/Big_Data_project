c07a9ec133c2ca057efb009156fb04a2eb8d0c40
jub205 <jub205@nyu.edu>
2017-04-15 15:07:19 -0400

    Corrected date issue

--- src/visualize_result.py
+++ src/visualize_result.py
@@ -1,10 +1,10 @@
+
 # coding: utf-8
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
-import matplotlib
-matplotlib.use('Agg')
+import numpy as np
 #Util functions
 def str2datetime(x):
 
@@ -22,17 +22,17 @@ def clean_zip(x):
 
     return x
 
-
 #Plot number of complaints by year
 year = pd.read_csv('../output/ycount.out', sep='\t', header=None)
 year.columns = ['year','count']
+year = year.sort_values('year')
 plt.figure(figsize=[12,12])
-plt.bar(year['year'],year['count'])
+plt.bar(np.arange(len(year['count'])),year['count'])
 plt.title('Number of complaints by year')
 plt.ylabel('Number of complaints')
+plt.xticks(np.arange(len(year['count'])),year['year'].apply(str))
 plt.xlabel('Year')
 plt.savefig('yearcount.png')
-
 
 #Plot number of complaints by date
 daily = pd.read_csv('../output/ymdcount.out', sep='\t', header=None)
@@ -58,8 +58,8 @@ for n,i in enumerate(years):
     data = data.sort_values('count',ascending=False)
     data = data[:10]
     plt.subplot(3,3,n+1)
-    plt.barh([j for j in range(len(data['count']))],data['count'])
-    plt.yticks([j for j in range(len(data['count']))],data['agency'])
+    plt.barh(np.arange(len(data['count'])),data['count'])
+    plt.yticks(np.arange(len(data['count'])),data['agency'])
     plt.title('Top 10 agencies with the most complaints in %s'%str(i))
     plt.xlabel('Count')
     plt.ylabel('Agency')
@@ -71,12 +71,15 @@ plt.figure(figsize=[28,24])
 plt.subplots_adjust(hspace=0.4)
 for n,i in enumerate(agencies):
     data = agency[agency['agency']==i].reset_index(drop=True)
+    data = data.sort_values('year')
     plt.subplot(7,4,n+1)
-    plt.bar(data['year'],data['count'])
+    plt.bar(np.arange(len(data['count'])),data['count'])
     plt.title("Yearly complaints to %s"%i)
     plt.xlabel("Year")
     plt.ylabel("Number of complaints")
+    plt.xticks(np.arange(len(data['count'])),data['year'].apply(str))
 plt.savefig('yearly_agency.png')
+
 
 #top 20 zipcode with the most complaints from 2009-2017
 zipyear = pd.read_csv('../output/zipcodey.out', sep='\t', header=None)
@@ -85,8 +88,8 @@ zipyear['zip'] = zipyear.zipcode.apply(lambda x: clean_zip(x))
 zipyear = zipyear[zipyear['zip']!='Invalid']
 top20 = zipyear.groupby(by=['zip'])['count'].sum().sort_values()[-20:]
 plt.figure(figsize=[12,12])
-plt.barh([i for i in range(20)],top20.values)
-plt.yticks([i for i in range(20)],top20.index)
+plt.barh(np.arange(20),top20.values)
+plt.yticks(np.arange(20),top20.index)
 plt.xlabel('Number of complaints')
 plt.ylabel('Zipcode')
 plt.title('Total number of complaints from 2009-2017')
@@ -97,11 +100,13 @@ plt.figure(figsize=[28,24])
 plt.subplots_adjust(hspace=0.4)
 for n,i in enumerate(top20.index):
     data = zipyear[zipyear['zip']==i].reset_index(drop=True)
+    data = data.sort_values('year')
     plt.subplot(7,4,n+1)
-    plt.bar(data['year'],data['count'])
+    plt.bar(np.arange(len(data['count'])),data['count'])
     plt.title("Yearly complaints in %s"%i)
     plt.xlabel("Year")
     plt.ylabel("Number of complaints")
+    plt.xticks(np.arange(len(data['count'])),data['year'].apply(str))
 plt.savefig('zip_year.png')
 
 #Number of complaints by borough and year
@@ -112,11 +117,13 @@ plt.figure(figsize=[28,24])
 plt.subplots_adjust(hspace=0.4)
 for n,i in enumerate(boroughs):
     data = borough[borough['borough']==i].reset_index(drop=True)
+    data = data.sort_values('year')
     plt.subplot(3,2,n+1)
-    plt.bar(data['year'],data['count'])
+    plt.bar(np.arange(len(data['count'])),data['count'])
     plt.title("Yearly complaints in %s"%i)
     plt.xlabel("Year")
     plt.ylabel("Number of complaints")
+    plt.xticks(np.arange(len(data['count'])),data['year'].apply(str))
 plt.savefig('borough_year.png')
 
 #top 20 city with the most complaints from 2009-2017
@@ -145,6 +152,5 @@ for n,i in enumerate(cyears):
     plt.title('Top 20 cities with the most complaints in %s'%str(i))
     plt.xlabel('Count')
     plt.ylabel('city')
-plt.savefig('cityyearly.png')
-
+plt.savefig('top20city.png')
 
