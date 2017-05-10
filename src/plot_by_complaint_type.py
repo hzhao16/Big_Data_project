361ef9375e09c0134687114afc631d0fce12f40a
wanghezhi <wanghezhi@wanghezhis-MacBook-Pro.local>
2017-05-10 16:23:39 -0400

    modifed the pont and color

--- src/plot_by_complaint_type.py
+++ src/plot_by_complaint_type.py
@@ -17,12 +17,12 @@ top10_count_number = count_percentage[:10]
 top10_count_complaint_type = complaint_type[:10]
 y_pos = np.arange(len(top10_count_complaint_type))
 
-plt.figure(figsize = (30, 18))
-plt.barh(y_pos, top10_count_number, align='center', color = 'b')
-plt.yticks(y_pos, top10_count_complaint_type, size='medium')
-plt.xlabel('Rate of Complaint Type', size = 'large')
-plt.ylabel('Complaint Type Count percentage', size = 'large')
-plt.title('Percentage of Top 10 Complaint Type Count')
+plt.figure(figsize = (36, 18))
+plt.barh(y_pos, top10_count_number, align='center', color = 'b', alpha = 0.5)
+plt.yticks(y_pos, top10_count_complaint_type, size='xx-large')
+plt.xlabel('Rate of Complaint Type', size = 'xx-large')
+plt.ylabel('Complaint Type Count percentage', size = 'xx-large')
+plt.title('Percentage of Top 10 Complaint Type Count', size = 'xx-large')
 plt.savefig('../plots/top_10_complaint_type.png')
 
 f1.close()
